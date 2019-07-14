037172c15c870349634a6f0284c0b6250f2a79a3
saharat wongyai <tkrunner30102@gmail.com>
2019-07-14 11:55:29 +0700

    -play

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/play/:num1/:num2", function(req, res, next) {
-  //console.log(req.params)
-  var num1 = req.params.num1;
-  var num2 = req.params.num2;
-  var sum = parseInt(num1) + parseInt(num2);
-  res.send("ผลรวมของตัวเลข 2 จำนวนคือ " + sum);
- });
  
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
